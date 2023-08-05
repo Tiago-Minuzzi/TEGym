81d92b08bd061149da28796fe463fbc197c6aed1
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-08-04 22:13:01 -0300

    Create class Preprocessor

--- preprocessing.py
+++ preprocessing.py
@@ -1,33 +1,46 @@
 import numpy as np
-from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.text import Tokenizer
 
 
-def kmerizer(sequencia: str, k: int = 4) -> np.ndarray:
-    """Creates a numpy array containing k-mers from a string"""
-    k_seq = np.array([sequencia[i:i+k] for i in range(len(sequencia)-k+1)])
-    return k_seq
+class Preprocessor:
+    def __init__(self, kmer_len: int = 4):
+        self.kmer_len = kmer_len
+        self.oov_tok: str = 'n' * kmer_len
 
+    def kmerizer(self, sequencia: str, k: int = None) -> np.ndarray:
+        """Creates a numpy array containing k-mers from a string"""
 
-def tokenizator(arr: list, max_words: int, oov_tok_len: int = 4):
-    oov = 'n' * oov_tok_len
-    tokenizer = Tokenizer(num_words=max_words, oov_token=oov)
-    tokenizer.fit_on_texts(arr)
+        if k is None:
+            k = self.kmer_len
 
-    # Tokenize os dados e converta em sequências numéricas
-    sequences = tokenizer.texts_to_sequences(arr)
-    return sequences
+        k_seq = [sequencia[i:i+k] for i in range(len(sequencia)-k+1)]
+        return k_seq
 
+    def tokenizator(self, arr: list, max_words: int = None):
+        """Transforms words/characters in numeric tokens"""
+        words = [j for i in arr for j in i]
 
-def zero_padder(arr: list, pad_len: int = None) -> np.ndarray:
-    """Appends zeros to inner arrays of list of arrays
-    to desire padding length. If no padding length is provided,
-    pads to the length of the longest array."""
-    # Get max length
-    max_len = max([len(i) for i in arr])
-    # If padding length is provided, max_len = pad_len
-    if pad_len:
-        max_len = pad_len
-    # Array padding
-    padded = np.array([np.hstack((a, np.zeros(max_len - len(a)))) for a in arr])
-    return padded
+        if not max_words:
+            max_words = len(set(words))
+
+        tokenizer = Tokenizer(num_words=max_words, oov_token=self.oov_tok)
+        tokenizer.fit_on_texts(words)
+
+        # Tokenize os dados e converta em sequências numéricas
+        sequences = tokenizer.texts_to_sequences(arr)
+        return sequences
+
+    def zero_padder(self, arr: list, pad_len: int = None) -> np.ndarray:
+        """Appends zeros to inner arrays of list of arrays
+        to desire padding length. If no padding length is provided,
+        pads to the length of the longest array."""
+        # Get max length
+        max_len = max([len(i) for i in arr])
+        # If padding length is provided, max_len = pad_len
+
+        if pad_len:
+            max_len = pad_len
+
+        # Array padding
+        padded = np.array([np.hstack((a, np.zeros(max_len - len(a)))) for a in arr])
+        return padded
