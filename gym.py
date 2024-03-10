8743a6343027448a74777ba045d3bf321de29bd6
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2024-03-09 21:38:08 -0300

    Run hyperparameters search if no external hyperparameters file

--- gym.py
+++ gym.py
@@ -1,16 +1,15 @@
+import time
 import argparse
+import subprocess
 import pandas as pd
 from pathlib import Path
 from neural_net import Trainer
 from preprocessing import Preprocessor
 
 # =============================================
-
-tr = Trainer()
-pp = Preprocessor()
-
+tr              = Trainer()
+pp              = Preprocessor()
 # =============================================
-
 # Helper
 parser  = argparse.ArgumentParser(prog          = 'gym.py',
                                   description   = "Train your own classifier model.")
@@ -36,6 +35,11 @@ parser.add_argument('-t',    '--title',
                     type    = str,
                     help    = 'Model identifier (optional).')
 
+parser.add_argument('-r',    '--runs',
+                    default = 20,
+                    type    = int,
+                    help    = 'number of runs (tests) to find the hyperparameters.')
+
 parser.add_argument('-s',    '--split',
                     default = 0.1,
                     type    = float,
@@ -43,13 +47,24 @@ parser.add_argument('-s',    '--split',
 
 args    = parser.parse_args()
 # =============================================
-# Files
-hyper_csv = Path(args.hyper)
-
-# =============================================
 # Variables
-metric      = args.metric
-sort_order  = True
+timestamp       = time.strftime('%y%m%d%H%M%S')
+metric          = args.metric
+sort_order      = True
+hyper_option    = '--fasta'
+model_title     = args.title
+n_runs          = args.runs
+n_split         = args.split
+# =============================================
+# Files
+if args.fasta:
+    infile = Path(args.fasta)
+elif args.csv:
+    infile = Path(args.csv)
+    hyper_option    = '--csv'
+hyper_csv   = args.hyper
+prefix      = f"{args.title}_{infile.stem}"
+hyper_out   = f"hyperparams_{prefix}_in_{timestamp}.csv"
 # =============================================
 if hyper_csv:
     hyper_df = pd.read_csv(hyper_csv)
@@ -61,3 +76,9 @@ if hyper_csv:
         metric = 'val_loss'
     hyper_df.sort_values(metric, inplace=True, ascending=sort_order)
     hp_dict = hyper_df.head(1).to_dict('records')[0]
+else:
+    subprocess.run(['python',       'hyperparameters.py',
+                    hyper_option,   infile,
+                    '--title',      model_title,
+                    '--runs',       str(n_runs),
+                    '--split',      str(n_split)])
