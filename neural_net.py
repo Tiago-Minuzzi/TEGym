e5f2ca49f77e329a0a474939cbf7d45d43ebe1a3
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2024-03-25 14:52:11 -0300

    Use parameters dictionary

--- neural_net.py
+++ neural_net.py
@@ -11,39 +11,39 @@ sys.stderr = stderr
 
 
 class Trainer:
-    def create_model(self, h_param: Dict[str, list], default_params: Dict[str, int], array_shape: int) -> Sequential:
+    def create_model(self, params_dict: Dict[str, list], array_shape: int) -> Sequential:
         model = Sequential()
 
-        model.add(Embedding(input_dim       = default_params["vocab_size"],
-                            output_dim      = default_params['emb_dim'],
+        model.add(Embedding(input_dim       = params_dict["vocab_size"],
+                            output_dim      = params_dict['emb_dim'],
                             input_length    = array_shape,
                             mask_zero       = False))
 
-        model.add(Conv1D(filters            = h_param['conv1'],
-                         kernel_size        = h_param['kernel_size'],
+        model.add(Conv1D(filters            = params_dict['conv1'],
+                         kernel_size        = params_dict['kernel_size'],
                          input_shape        = (array_shape, 1),
                          activation         = 'relu'))
-        model.add(MaxPooling1D(pool_size    = h_param['pool_size']))
+        model.add(MaxPooling1D(pool_size    = params_dict['pool_size']))
 
-        model.add(Conv1D(filters            = h_param['convN'],
-                         kernel_size        = h_param['kernel_size'],
+        model.add(Conv1D(filters            = params_dict['convN'],
+                         kernel_size        = params_dict['kernel_size'],
                          activation         = 'relu'))
-        model.add(MaxPooling1D(pool_size    = h_param['pool_size']))
+        model.add(MaxPooling1D(pool_size    = params_dict['pool_size']))
 
-        model.add(Conv1D(filters            = h_param['convN'],
-                         kernel_size        = h_param['kernel_size'],
+        model.add(Conv1D(filters            = params_dict['convN'],
+                         kernel_size        = params_dict['kernel_size'],
                          activation         = 'relu'))
-        model.add(MaxPooling1D(pool_size    = h_param['pool_size']))
+        model.add(MaxPooling1D(pool_size    = params_dict['pool_size']))
 
         model.add(LayerNormalization())
         model.add(Flatten())
 
-        model.add(Dense(h_param['dense_neuron'],
+        model.add(Dense(params_dict['dense_neuron'],
                         activation = 'relu'))
 
-        model.add(Dropout(h_param["dropout"]))
+        model.add(Dropout(params_dict["dropout"]))
 
-        model.add(Dense(default_params["vocab_size_lab"],
+        model.add(Dense(params_dict["vocab_size_lab"],
                         activation = 'softmax'))
 
         model.compile(loss      = 'categorical_crossentropy',
@@ -52,11 +52,11 @@ class Trainer:
 
         return model
 
-    def model_fit(self, model, X_train, y_train, X_test, y_test, n_epochs, n_batch, c_weights):
+    def model_fit(self, model, params_dict, X_train, y_train, X_test, y_test, n_epochs, n_batch, c_weights):
         history = model.fit(X_train,
                             y_train,
-                            epochs          = n_epochs,
-                            batch_size      = n_batch,
+                            epochs          = params_dict['epochs'],
+                            batch_size      = params_dict['batch_size'],
                             validation_data = (X_test, y_test),
                             verbose         = 1,
                             shuffle         = True,
