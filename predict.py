72a2c7358fe1243e0eddb755203b8ef22424f75a
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2024-03-26 21:17:54 -0300

    Read TOML with model info

--- predict.py
+++ predict.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import toml
 import pandas as pd
 from numpy import argmax
 from pathlib import Path
@@ -16,12 +17,13 @@ sys.stderr = stderr
 
 input_fasta     = Path(sys.argv[1])
 timestamp       = time.strftime('%y%m%d%H%M%S')
-output_table    = f"{input_fasta.stem}_{timestamp}.csv"
+output_table    = f"{input_fasta.stem}_{timestamp}_prediction.csv"
 model           = load_model(sys.argv[2])
+model_info      = Path(sys.argv[3])
 
 pp = Preprocessor()
 
-with open(input_fasta) as fa:
+with open(input_fasta) as fa, open(model_info) as tmf:
     fids = []
     fsqs = []
     print("### Loading sequences ###")
@@ -30,14 +32,20 @@ with open(input_fasta) as fa:
         fids.append(fid)
         fsqs.append(fsq)
 
+    tmf         = toml.load(tmf)
+    tmf_dict    = tmf['model_info']
+    labels      = tmf_dict['Labels']
+    labels_dict = {k: v for k, v in enumerate(labels)}
+
     print("### Tokenizing and padding sequences ###\n")
     fsqs = pp.zero_padder(list(map(pp.seq_tokenizer, fsqs)))
 
     print("### Starting prediction ###")
     predictions = model.predict(fsqs)
     pred_labels = argmax(predictions, axis=1)
-    df          = pd.DataFrame(predictions).round(4)
+    df          = pd.DataFrame(predictions, columns=labels).round(4)
     df.insert(0, 'id', pd.Series(fids))
     df.insert(1, 'prediction', pd.Series(pred_labels))
+    df['prediction'] = df['prediction'].map(labels_dict)
     df.to_csv(output_table, index=False)
     print(">>> Done!")
