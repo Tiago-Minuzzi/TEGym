1ca562916d31aa1339483d9aa766621c3356fd73
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2024-03-27 08:45:24 -0300

    Flag parser for predict.py

--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import toml
+import argparse
 import pandas as pd
 from numpy import argmax
 from pathlib import Path
@@ -15,12 +16,31 @@ from preprocessing import Preprocessor
 from tensorflow.keras.models import load_model
 sys.stderr = stderr
 
-input_fasta     = Path(sys.argv[1])
+# =============================================
+parser = argparse.ArgumentParser(prog           = 'predict.py',
+                                 description    = "Predict using your trained model.")
+
+parser.add_argument("-f", "--fasta",
+                    required    = True,
+                    help        = "Input FASTA file containing the sequences to be classified.")
+
+parser.add_argument("-m", "--model",
+                    required    = True,
+                    help        = "Path to the trained model.")
+
+parser.add_argument("-i", "--info_file",
+                    required    = True,
+                    help        = "Path to the TOML file containing model information.")
+
+args = parser.parse_args()
+# =============================================
+input_fasta     = Path(args.fasta)
 timestamp       = time.strftime('%y%m%d%H%M%S')
 output_table    = f"{input_fasta.stem}_{timestamp}_prediction.csv"
-model           = load_model(sys.argv[2])
-model_info      = Path(sys.argv[3])
+model           = load_model(args.model)
+model_info      = Path(args.info_file)
 
+# =============================================
 pp = Preprocessor()
 
 with open(input_fasta) as fa, open(model_info) as tmf:
@@ -32,16 +52,19 @@ with open(input_fasta) as fa, open(model_info) as tmf:
         fids.append(fid)
         fsqs.append(fsq)
 
+# =============================================
     tmf         = toml.load(tmf)
     tmf_dict    = tmf['model_info']
     labels      = tmf_dict['Labels']
     labels_dict = {k: v for k, v in enumerate(labels)}
     seq_len     = tmf_dict['Maximum_length']
 
+# =============================================
     print("### Tokenizing and padding sequences ###\n")
     fsqs = pp.zero_padder(list(map(pp.seq_tokenizer, fsqs)),
                           seq_len)
 
+# =============================================
     print("### Starting prediction ###")
     predictions = model.predict(fsqs)
     pred_labels = argmax(predictions, axis=1)
@@ -51,3 +74,5 @@ with open(input_fasta) as fa, open(model_info) as tmf:
     df['prediction'] = df['prediction'].map(labels_dict)
     df.to_csv(output_table, index=False)
     print(">>> Done!")
+    print(f">>> Results saved to {output_table}.")
+# =============================================
